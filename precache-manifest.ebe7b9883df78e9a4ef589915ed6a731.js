747d1f446bacd516eceeecbf6a6dfce73b512cec
Ravikesh Yadav <ravikesh@netzary.com>
2020-08-24 23:25:50 +0530

    Updates

---
+++ precache-manifest.ebe7b9883df78e9a4ef589915ed6a731.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ac6fc6ec5dc08f9e819dbda85f196c",
+    "url": "/dehnovo/index.html"
+  },
+  {
+    "revision": "8a1464f7281fefd6fff8",
+    "url": "/dehnovo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d58e64bfa1b19cad8278",
+    "url": "/dehnovo/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "8a1464f7281fefd6fff8",
+    "url": "/dehnovo/static/js/2.b1f9c1e3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dehnovo/static/js/2.b1f9c1e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d58e64bfa1b19cad8278",
+    "url": "/dehnovo/static/js/main.57a0f869.chunk.js"
+  },
+  {
+    "revision": "3a381a949cfe0d34131f",
+    "url": "/dehnovo/static/js/runtime-main.b820ca95.js"
+  },
+  {
+    "revision": "31edb7256f3475a9838664be3687f0c3",
+    "url": "/dehnovo/static/media/header.31edb725.jpg"
+  },
+  {
+    "revision": "211c127cf507a77d8408d666de206d5d",
+    "url": "/dehnovo/static/media/meet.211c127c.jpg"
+  },
+  {
+    "revision": "ec4e5d118dc60c6e8891d30e46c91cdc",
+    "url": "/dehnovo/static/media/requirement.ec4e5d11.jpg"
+  },
+  {
+    "revision": "8f795b2e7d43b7074bdcaaa11fc8f75e",
+    "url": "/dehnovo/static/media/why.8f795b2e.jpg"
+  }
+]);
